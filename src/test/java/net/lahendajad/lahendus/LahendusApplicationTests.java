3c9509982d7e42fac9c54b25324f2fe2bc9c0db6
PriitPaluoja <priitpaluoja@gmail.com>
2018-04-29 14:39:18 +0300

    initial

---
+++ src/test/java/net/lahendajad/lahendus/LahendusApplicationTests.java
@@ -0,0 +1,16 @@
+package net.lahendajad.lahendus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LahendusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
