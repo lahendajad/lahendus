6104ea737ae8aad5a1981575b9fc5b33a73c4b90
Priit Paluoja <priitpaluoja@gmail.com>
2018-09-04 18:32:22 +0300

    JWT Cron job

--- api/src/main/kotlin/net/lahendus/auth/TokenWhitelist.kt
+++ api/src/main/kotlin/net/lahendus/auth/TokenWhitelist.kt
@@ -1,11 +1,30 @@
 package net.lahendus.auth
 
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Jwts
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.util.concurrent.ConcurrentSkipListSet
 
 @Service
 class TokenWhitelist {
     private val tokens: MutableSet<String> = ConcurrentSkipListSet()
+
+    // Every night at 03:00 clear expired tokens
+    @Scheduled(cron = "0 0 3 * * *", zone = "Europe/Athens")
+    fun clearExpiredTokens() {
+        val toBeRemoved = mutableListOf<String>()
+
+        tokens.forEach {
+            try {
+                Jwts.parser().setSigningKey(JWTController.KEY).parseClaimsJws(it)
+            } catch (e: ExpiredJwtException) {
+                toBeRemoved.add(it)
+            }
+        }
+
+        toBeRemoved.forEach { removeToken(it) }
+    }
 
     fun tokenIsAllowed(token: String): Boolean = tokens.contains(token)
 
